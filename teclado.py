ed8dd9a385cafcf97e3680d0ba8592356e61d1f8
slow <96095769+scriptkid2222@users.noreply.github.com>
2022-09-22 05:28:26 -0300

    Add files via upload

---
+++ teclado.py
@@ -0,0 +1,29 @@
+
+from re import T
+from tkinter import E
+from webbrowser import Chrome
+import pyautogui
+from time import sleep
+
+pyautogui.confirm(' Script')
+
+
+pyautogui.write ('I only know how to create something or am I a copy', interval=1)
+pyautogui.hotkey('enter')
+pyautogui.ckick('1157', '850')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
